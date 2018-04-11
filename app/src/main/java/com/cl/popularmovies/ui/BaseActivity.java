019bf0e2f4c2f1281f650a73ff0603af701ca67c
CarlosYang <mrcarlosyang@gmail.com>
2018-04-10 21:24:19 -0400

    chore:Update to latest gradle

--- app/src/main/java/com/cl/popularmovies/ui/BaseActivity.java
+++ app/src/main/java/com/cl/popularmovies/ui/BaseActivity.java
@@ -11,4 +11,30 @@ public class BaseActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
 
     }
+
+    public static String compress(String input){
+        if (input==null||input.isEmpty()) {
+            return "";
+        }
+        StringBuffer stringBuffer=new StringBuffer();
+        char countChar=0;
+        int count=0;
+        for (int i = 0; i < input.length(); i++) {
+            if (input.charAt(i) == countChar) {
+                count++;
+            } else {
+                if (countChar!=0) {
+                    stringBuffer.append(countChar);
+                    stringBuffer.append(count);
+                }
+                countChar=input.charAt(i);
+                count=1;
+            }
+        }
+        if (countChar!=0) {
+            stringBuffer.append(countChar);
+            stringBuffer.append(count);
+        }
+        return stringBuffer.length()<input.length()?stringBuffer.toString():input;
+    }
 }
